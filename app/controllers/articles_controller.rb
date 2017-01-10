c931c735fd15a884ca71835679ddaba595448296
Peter <peto.mester@gmail.com>
2017-01-10 14:01:54 +0100

    finished blog comments feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,13 @@
 class ArticlesController < ApplicationController
   include ArticlesHelper
+
   def index
     @articles = Article.all
   end
   def show
     @article = Article.find(params[:id])
+    @comment = Comment.new
+    @comment.article_id = @article.id
   end
   def new
     @article = Article.new
